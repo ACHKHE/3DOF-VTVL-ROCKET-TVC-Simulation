3ff7b21fc8bbe36cf0ed6d9c17c1262fafb9daa3
ACHKHE <achrafkhelefi1888@gmail.com>
2024-12-25 11:48:07 +0000

    new function added

--- test.cpp
+++ test.cpp
@@ -1,5 +1,9 @@
+#include <iostream>
 
-int c=9;
-char x=achraf;
-for i=0;10;i++
+int c = 9; // Global variable (unused in this example)
 
+int main() {
+    char x = 'a'; // Correctly initializing x with a character
+    std::cout << "Hello Achraf" << std::endl;
+    return 0; // Returning 0 to indicate successful execution
+}
